f205306b47caa6138985f3498a5ff44e43d206eb
jackie jan <zakaria.farah55@outlook.com>
2025-06-03 09:18:46 +0300

    wip

--- pipe.h
+++ pipe.h
@@ -1,10 +1,19 @@
 #ifndef PIPE_H
 #define PIPE_H
 
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "./libft/libft.h"
+#include <fcntl.h>
+
 typedef struct s_cmd
 {
     char *name;
     char *flags;
 } t_cmd;
+
+int read_stdin(char **buff);
+int read_write_fd(int read_fd, int write_fd);
 
 #endif
