83ea24725c369d888436be679716a4789c63bd7b
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-04 20:03:27 +0300

    added functions defenitions

--- pipe.h
+++ pipe.h
@@ -7,13 +7,29 @@
 #include "./libft/libft.h"
 #include <fcntl.h>
 
+typedef enum s_cmd_type
+{
+    INFILE,
+    OUTFILE,
+    CMD,
+} e_command_type;
+
 typedef struct s_cmd
 {
     char *name;
-    char *flags;
+    char *cmd;
+    e_command_type type;
 } t_cmd;
+
+
 
 int read_stdin(char **buff);
 int read_write_fd(int read_fd, int write_fd);
+t_list *process(int argc, char **args);
+
+void pipex(int argc, t_list *args);
+
+// helpers
+void print_cmd_list(t_list *head);
 
 #endif
