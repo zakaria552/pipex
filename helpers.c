2162aacd4654f7d76de9987c73296e3f3faf2ef2
jackie jan <zakaria.farah55@outlook.com>
2025-06-05 20:42:49 +0300

    changed so that correctly prints the commands stuct

--- helpers.c
+++ helpers.c
@@ -11,17 +11,41 @@ char *get_enum_type(e_command_type type)
 		return "UNKNOWN";
 }
 
+void print_cmd(t_cmd *cmds)
+{
+	int j;
+	char **args = cmds->cmd;
+
+	j = 0;
+		ft_printf("[cmd: ");
+		while(args && args[j])
+		{
+			ft_printf("%s ", args[j]);
+			j++;
+		}
+		ft_printf(", name: %s, type: %s]\n", cmds->path_name, get_enum_type(cmds->type));
+}
 
 void print_cmd_list(t_list *head)
 {
 	t_cmd *cmds;
 	int i;
-
+	int j;
+	char **args;
 	i = 0;
+
 	while(head != NULL)
 	{
 		cmds = head->content;
-		ft_printf("--%d-->[cmd: %s, name: %s, type: %s]\n", i, cmds->cmd, cmds->name, get_enum_type(cmds->type));
+		args = cmds->cmd;
+		j = 0;
+		ft_printf("--%d-->[cmd: ", i);
+		while(args && args[j])
+		{
+			ft_printf("%s ", args[j]);
+			j++;
+		}
+		ft_printf(", name: %s, type: %s]\n", cmds->path_name, get_enum_type(cmds->type));
 		head = head->next;
 		i++;
 	}
