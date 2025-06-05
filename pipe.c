c8d0801e81ed5d0a5bdf17b2ad2077f1e61402ff
jackie jan <zakaria.farah55@outlook.com>
2025-06-05 20:47:19 +0300

    now able to execute the first command passed as argument

--- pipe.c
+++ pipe.c
@@ -122,6 +122,7 @@ void list_open_fds(char *from, int index) {
         }
     }
 }
+
 bool map_infile_to_pipe(t_list **args, int pipe_read_end)
 {
     t_cmd *cmd;
@@ -130,7 +131,7 @@ bool map_infile_to_pipe(t_list **args, int pipe_read_end)
     
     list_open_fds("Parent", 0);
     cmd = (*args)->content;
-    fd = open(cmd->name, O_RDONLY);
+    fd = open(cmd->path_name, O_RDONLY);
     if (fd < 0)
         return false;
     dup2(fd, pipe_read_end);
@@ -145,14 +146,13 @@ bool map_infile_to_pipe(t_list **args, int pipe_read_end)
 
 void close_pipe(int pipe[2])
 {
-    printf("Closing pipe[%d][%d]", pipe[0], pipe[1]);
+    // printf("Closing pipe[%d][%d]", pipe[0], pipe[1]);
     close(pipe[0]);
     close(pipe[1]);
 }
 
 void pipex(int argc, t_list *args)
 {
-    t_cmd *cmd;
     int pipes[argc - 2][2];
     int i;
 
@@ -160,18 +160,23 @@ void pipex(int argc, t_list *args)
         return;
     print_cmd_list(args);
     i = 0;
-    while(args)
+    while(args && args->next != NULL)
     {
         printf("[Parent] - iteration-%d\n", i);
         pipe(pipes[i+1]);
-        // if (pipe(pipes[i+1]) < 0)
-        // if (fork() == 0)
-        //     //execute_cmd();
+        if (pipe(pipes[i+1]) < 0)
+            exit(1);
+        if (fork() == 0)
+        {
+            printf("[child-%d] Executing command\n", i);
+            execute(args->content, pipes[i], pipes[i+1]);
+        }
         close_pipe(pipes[i]);
+        wait(NULL);
+        exit(0);
         args = args->next;
         i++;
     }
     close_pipe(pipes[i]);
-    list_open_fds("Parent", 0);
-
+    // list_open_fds("Parent", 0);
 }
