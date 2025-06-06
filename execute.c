9b526435d14c9bdf65dd5e614933f0b38b46bcc2
jackie jan <zakaria.farah55@outlook.com>
2025-06-06 20:05:23 +0300

    wip

--- execute.c
+++ execute.c
@@ -1,16 +1,17 @@
 #include "pipe.h"
 
-void execute(t_cmd *command, int pipe[2], int next_pipe[2])
+void execute(t_cmd *command, int pipe[2], int next_pipe[2], int i)
 {
-    dup2(pipe[0], STDIN_FILENO);
-    // dup2(STDOUT_FILENO, next_pipe[1]);
-    // close_pipe(pipe);
+    char *envp[] = { "PATH=/bin", NULL};
+    
     print_cmd(command);
-    // printf("stdin-->[");
-    // printf("]--end");
-    // printf("bytes_written: %zu\n", read_write_file(pipe[0], 1));
-    // printf("pipe-[%d][%d]\n", pipe[0], pipe[1]);
-    execv(command->path_name, command->cmd);
-    printf("Failed to execute-->");
+    ft_printf("Current pipe-fds[%d][%d] --- next-[%d][%d]\n", pipe[0], pipe[1], next_pipe[0], next_pipe[1]);
+    dup2(pipe[0], STDIN_FILENO);
+    dup2(next_pipe[1], STDOUT_FILENO);
+    close_pipe(pipe);
+    close_pipe(next_pipe);
+    execve(command->path_name, command->cmd, envp);
+    perror("\nErrored");
+    ft_printf("Failed to execute-->[cmd: %s]", *command->cmd);
     exit(1);
 }
