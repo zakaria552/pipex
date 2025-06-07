da11edad20fb6485cd7a288ba82b5dbd023a2b02
jackie jan <zakaria.farah55@outlook.com>
2025-06-07 22:19:16 +0300

    now able to handle here doc argument

--- pipe.c
+++ pipe.c
@@ -11,7 +11,29 @@ void list_open_fds(char *from, int index) {
     }
 }
 
-bool map_infile_to_pipe(t_list **args, int pipe_read_end)
+bool map_stdin_to_pipe(char *limiter, int pipe[0])
+{
+    char *line;
+
+    while (true)
+    {
+        ft_printf("heredoc> ");
+        line = get_next_line(STDIN_FILENO);
+        if (!line)
+            return false;
+        if (ft_strncmp(line, limiter, ft_strlen(limiter)) == 0)
+            break;
+        if (write(pipe[1], line, ft_strlen(line)) < 0)
+        {
+            free(line);
+            return false;
+        }
+        free(line);
+    }
+    free(line);
+    return true;
+}
+bool map_infile_to_pipe(t_list **args, int pipe[2])
 {
     t_cmd *cmd;
     t_list *tmp;
@@ -19,10 +41,15 @@ bool map_infile_to_pipe(t_list **args, int pipe_read_end)
     
     list_open_fds("Parent", 0);
     cmd = (*args)->content;
+    if (cmd->type == HERE_DOC)
+    {
+        *args = (*args)->next;
+        return map_stdin_to_pipe(cmd->path_name, pipe);
+    }
     fd = open(cmd->path_name, O_RDONLY);
     if (fd < 0)
         return false;
-    dup2(fd, pipe_read_end);
+    dup2(fd, pipe[0]);
     close(fd);
     if (fd < 0)
         return false;
@@ -44,7 +71,7 @@ void pipex(int argc, t_list *args)
     int pipes[argc - 2][2];
     int i;
 
-    if (pipe(pipes[0]) < 0 || !map_infile_to_pipe(&args, pipes[0][0]))
+    if (pipe(pipes[0]) < 0 || !map_infile_to_pipe(&args, pipes[0]))
         return;
     print_cmd_list(args);
     i = 0;
