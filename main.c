b8fe8deba39e4590ed800e0c90710929d34a22b5
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-09 14:07:27 +0300

    passing the current num of commands to pipex

--- main.c
+++ main.c
@@ -1,26 +1,42 @@
 #include "pipe.h"
 void free_cmd(t_cmd *cmd)
 {
-    free(cmd->path_name);
-    free(cmd->cmd);
+	int i;
+    char **cmd_flags;
+    
+    cmd_flags = cmd->cmd;
+	i = 0;
+	while(cmd_flags && cmd_flags[i])
+    {
+		free(cmd_flags[i]);
+        i++;
+    }
+    free(cmd_flags);
+	free(cmd->path_name);
+    free(cmd);
 }
 int main(int argc, char **argv)
 {
     t_list *args_list;
-    
-	if (argc < 5)
-    {
-        perror("Must have atleast 4 arguments");
-        exit(-1);
-    }
+    int num_cmd;
+
     args_list = process(argc, argv);
     if (!args_list)
     {
         perror("Failed to process args");
         exit(-1);
     }
+    num_cmd = ft_lstsize(args_list);
+	if (num_cmd < 4)
+    {
+        perror("Must have atleast 4 arguments, (here_doc LIMITTER is treated as one argument)");
+        ft_lstclear(&args_list, free_cmd);
+        exit(1);
+    }
     print_cmd_list(args_list);
-    pipex(argc, args_list);
+    list_open_fds("Parent", 1);
+    pipex(num_cmd, args_list);
+    list_open_fds("Parent", 1);
     ft_lstclear(&args_list, free_cmd);
     return 0;
 }
