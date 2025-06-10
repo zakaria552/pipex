146bbded15a23c260a318c0a09062c0210ecec28
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-10 15:15:43 +0300

    Moved all the io related utils

---
+++ io_utils.c
@@ -0,0 +1,47 @@
+#include "pipe.h"
+
+ssize_t read_write_file(int fd, int write_fd)
+{
+    char buff[BUFFER_SIZE + 1];
+    ssize_t written_bytes;
+    ssize_t bytes_read;
+    
+    ft_memset(buff, 0, BUFFER_SIZE);
+    bytes_read = read(fd, buff, BUFFER_SIZE);
+    written_bytes = bytes_read;
+    while (bytes_read > 0)
+    {
+        if (write(write_fd, buff, bytes_read) < 0)
+            return -1;
+        ft_memset(buff, 0, bytes_read);
+        bytes_read = read(fd, buff, BUFFER_SIZE);
+        written_bytes+= bytes_read;
+    }
+    if (bytes_read < 0 )
+        return -1;
+    return (written_bytes);
+}
+
+void dump_to_outfile(t_cmd *cmd, int pipe[2])
+{
+    int fd;
+
+    if (unlink(cmd->path_name) < 0 && errno != ENOENT)
+    {
+       ft_printf("Failed to unlink file %s: %s ", cmd->path_name, errno);
+       close_pipe(pipe);
+       return;
+    }
+    fd = open(cmd->path_name, O_CREAT | O_WRONLY, 0644);
+    if (fd < 0)
+    {
+        close_pipe(pipe);
+        ft_printf("Failed to open file %s: %s", cmd->path_name, strerror(errno));
+        return ;
+    }
+    close(pipe[1]);
+    if (read_write_file(pipe[0], fd) < 0)
+        ft_printf("Error: %s", strerror(errno));
+    close(fd);
+    close(pipe[0]);
+}
