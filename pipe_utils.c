3bd789877ce5c5929d2ccd0e866502ebce33533a
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-10 15:16:06 +0300

    added all the pipe related utils

---
+++ pipe_utils.c
@@ -0,0 +1,18 @@
+#include "pipe.h"
+
+void close_pipe(int pipe[2])
+{
+    close(pipe[0]);
+    close(pipe[1]);
+}
+
+
+void print_here_doc_input_msg(int num_pipes)
+{
+    int i;
+
+    i = -1;
+    while (++i < num_pipes)
+        ft_printf("pipe ");
+    ft_printf("heredoc> ");
+}
