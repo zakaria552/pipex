369fe901f4ecd5a8a5e92611cf632f142fedfc12
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-10 15:17:09 +0300

    reorganized

--- pipe.h
+++ pipe.h
@@ -4,8 +4,9 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "./libft/libft.h"
 #include <fcntl.h>
+#include <string.h>
+#include "./libft/libft.h"
 
 typedef enum s_cmd_type
 {
@@ -23,20 +24,27 @@ typedef struct s_cmd
 } t_cmd;
 
 
-
-int read_stdin(char **buff);
-int read_write_fd(int read_fd, int write_fd);
+// processor
 t_list *process(int argc, char **args);
+void free_cmd(t_cmd *cmd);
 
+// pipe
 void pipex(int num_cmd, t_list *args);
+
+// pipe utils
 void close_pipe(int pipe[2]);
-void list_open_fds(char *from, int index);
+void print_here_doc_input_msg(int num_pipes);
 
 // helpers
 void print_cmd_list(t_list *head);
 void print_cmd(t_cmd *cmds);
+void list_open_fds(char *from, int index);
 
+// io_utils
 ssize_t read_write_file(int fd, int write_fd);
+void dump_to_outfile(t_cmd *cmd, int pipe[2]);
+
+// execute
 void execute(t_cmd *command, int pipe[2], int next_pipe[2], int i);
 
 #endif
