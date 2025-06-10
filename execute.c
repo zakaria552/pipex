2484ec3e8cb466dfba2b850011dc539776f714be
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-10 14:22:09 +0300

    removed printfs

--- execute.c
+++ execute.c
@@ -2,10 +2,8 @@
 
 void execute(t_cmd *command, int pipe[2], int next_pipe[2], int i)
 {
-    char *envp[] = { "PATH=/bin", NULL};
+    char *envp[] = { "PATH=/bin/", NULL};
     
-    print_cmd(command);
-    ft_printf("Current pipe-fds[%d][%d] --- next-[%d][%d]\n", pipe[0], pipe[1], next_pipe[0], next_pipe[1]);
     dup2(pipe[0], STDIN_FILENO);
     dup2(next_pipe[1], STDOUT_FILENO);
     close_pipe(pipe);
