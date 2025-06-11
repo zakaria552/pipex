b995eb0216af38c366d040d2baeeeaf0567f87be
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-11 19:18:15 +0300

    freeing memory when execve fails

--- execute.c
+++ execute.c
@@ -1,13 +1,22 @@
 #include "pipe.h"
 
-void execute(t_cmd *command, int pipe[2], int next_pipe[2], int i)
+void execute(t_cmd *command, int pipes[][2], t_list **args, int *pids)
 {
     char *envp[] = { "PATH=/bin/", NULL};
-    
-    dup2(pipe[0], STDIN_FILENO);
-    dup2(next_pipe[1], STDOUT_FILENO);
-    close_pipe(pipe);
+    int *current_pipe;
+    int *next_pipe;
+
+    current_pipe = pipes[0];
+    next_pipe = pipes[1];
+    if (dup2(current_pipe[0], STDIN_FILENO) < 0 || dup2(next_pipe[1], STDOUT_FILENO) < 0)
+    {
+        free(pids);
+        exit_err("Pipex: ", args, errno);
+    }
+    close_pipe(current_pipe);
     close_pipe(next_pipe);
     execve(command->path_name, command->cmd, envp);
-    exit(1);
+    ft_lstclear(args, (void *)free_cmd);
+    free(pids);
+    exit(errno);
 }
