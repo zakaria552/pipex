ea9215d798d52c9d38f405de551e4a7e528fa6ff
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-11 21:29:19 +0300

    Norm fixes and added 42 header

--- execute.c
+++ execute.c
@@ -1,22 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 21:25:07 by zfarah            #+#    #+#             */
+/*   Updated: 2025/06/11 21:28:49 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "pipe.h"
 
-void execute(t_cmd *command, int pipes[][2], t_list **args, int *pids)
+void	execute(t_cmd *command, int pipes[][2], t_list **args, int *pids)
 {
-    char *envp[] = { "PATH=/bin/", NULL};
-    int *current_pipe;
-    int *next_pipe;
+	const char	*envp[] = {"PATH=/bin/", NULL};
+	int			*current_pipe;
+	int			*next_pipe;
 
-    current_pipe = pipes[0];
-    next_pipe = pipes[1];
-    if (dup2(current_pipe[0], STDIN_FILENO) < 0 || dup2(next_pipe[1], STDOUT_FILENO) < 0)
-    {
-        free(pids);
-        exit_err("Pipex: ", args, errno);
-    }
-    close_pipe(current_pipe);
-    close_pipe(next_pipe);
-    execve(command->path_name, command->cmd, envp);
-    ft_lstclear(args, (void *)free_cmd);
-    free(pids);
-    exit(errno);
+	current_pipe = pipes[0];
+	next_pipe = pipes[1];
+	if (dup2(current_pipe[0], STDIN_FILENO) < 0 || dup2(next_pipe[1],
+			STDOUT_FILENO) < 0)
+	{
+		free(pids);
+		exit_err("Pipex: ", args, errno);
+	}
+	close_pipe(current_pipe);
+	close_pipe(next_pipe);
+	execve(command->path_name, command->cmd, envp);
+	ft_lstclear(args, (void *)free_cmd);
+	free(pids);
+	exit(errno);
 }
