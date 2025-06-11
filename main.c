6ffbdca07a20ef55935d779abcb267144acdcdef
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-11 19:17:19 +0300

    Reorganized to handle error and memory leaks

--- main.c
+++ main.c
@@ -3,34 +3,34 @@
 void wait_for_child_processes(int *pids, int num_process, int *status)
 {
     int i;
-
     i = -1;
     while (++i < num_process)
+    {
+        ft_printf("%d: , id: %d\n", i, pids[i]);
         waitpid(pids[i], status, 0);
+    }
 }
 
-int main(int argc, char **argv, char **envp)
+int main(int argc, char **argv)
 {
     t_list *args_list;
     int num_cmd;
-    int pids[argc - 3];
+    int *pids;
     int status;
 
     args_list = process(argc, argv);
-    if (!args_list)
-    {
-        perror("Failed to process args");
-        exit(-1);
-    }
     num_cmd = ft_lstsize(args_list);
 	if (num_cmd < 4)
-    {
-        perror("Must have atleast 4 arguments, (here_doc LIMITTER is treated as one argument)");
-        ft_lstclear(&args_list, (void *)free_cmd);
-        exit(1);
-    }
-    pipex(num_cmd, args_list, pids);
+        exit_err("Pipex: must have atleast 4 arguments, (here_doc LIMITTER is treated as one argument)", &args_list, EINVAL);
+    if (!args_list)
+        exit_err("Pipex: Failed to process args", NULL, errno);
+    pids = ft_calloc(num_cmd - 1, sizeof(int));
+    if (!pids)
+        exit_err("Pipex: Failed to allocate resource", &args_list, errno);
+    pipex(num_cmd, &args_list, pids);
+    print_cmd_list(args_list);
     ft_lstclear(&args_list, (void *)free_cmd);
-    wait_for_child_processes(pids, argc - 3, &status);
+    wait_for_child_processes(pids, num_cmd - 2, &status);
+    free(pids);
     return WEXITSTATUS(status);
 }
