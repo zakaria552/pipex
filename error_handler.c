12348b8ee7e382ce0b2b2bcacab31ce20c5a4377
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-11 19:41:59 +0300

    improved the behaviour of exit_err

--- error_handler.c
+++ error_handler.c
@@ -6,5 +6,14 @@ void pipe_error(char *msg, int current_pipe[2], int next_pipe[2])
 		close_pipe(current_pipe);
 	if (next_pipe)
 		close_pipe(next_pipe);
-	ft_printf("Pipex: %s", msg, strerror(errno));
+	perror("Pipex: %s");
+}
+
+void exit_err(char *msg, t_list **cmd_list, int err_code)
+{
+	errno = err_code;
+	if (cmd_list)
+		ft_lstclear(cmd_list, (void *) free_cmd);
+	perror(msg);
+	exit(errno);
 }
