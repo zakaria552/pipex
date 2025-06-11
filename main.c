2470f7f31f3b728c82b002b53eb433496c210a6a
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-11 19:38:46 +0300

    Renamed the processed args

--- main.c
+++ main.c
@@ -13,23 +13,23 @@ void wait_for_child_processes(int *pids, int num_process, int *status)
 
 int main(int argc, char **argv)
 {
-    t_list *args_list;
+    t_list *cmd_list;
     int num_cmd;
     int *pids;
     int status;
 
-    args_list = process(argc, argv);
-    if (!args_list)
+    cmd_list = process(argc, argv);
+    if (!cmd_list)
         exit_err("Pipex: Failed to process args", NULL, errno);
-    num_cmd = ft_lstsize(args_list);
+    num_cmd = ft_lstsize(cmd_list);
 	if (num_cmd < 4)
-        exit_err("Pipex: must have atleast 4 arguments, (here_doc LIMITTER is treated as one argument)", &args_list, EINVAL);
+        exit_err("Pipex: must have atleast 4 arguments, (here_doc LIMITTER is treated as one argument)", &cmd_list, EINVAL);
     pids = ft_calloc(num_cmd - 1, sizeof(int));
     if (!pids)
-        exit_err("Pipex: Failed to allocate resource", &args_list, ENOMEM);
-    pipex(num_cmd, &args_list, pids);
-    print_cmd_list(args_list);
-    ft_lstclear(&args_list, (void *)free_cmd);
+        exit_err("Pipex: Failed to allocate resource", &cmd_list, ENOMEM);
+    pipex(num_cmd, &cmd_list, pids);
+    print_cmd_list(cmd_list);
+    ft_lstclear(&cmd_list, (void *)free_cmd);
     wait_for_child_processes(pids, num_cmd - 2, &status);
     free(pids);
     return WEXITSTATUS(status);
