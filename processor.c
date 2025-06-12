f9b4280e42995b8c0de1bc14c245b01202c15eb6
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-12 17:08:50 +0300

    Moved the format_path into its own file

--- processor.c
+++ processor.c
@@ -6,18 +6,17 @@
 /*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 20:17:10 by zfarah            #+#    #+#             */
-/*   Updated: 2025/06/11 21:38:46 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/06/12 16:54:54 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipe.h"
 
-static t_list	*process_commands(char *command);
+static t_list	*process_commands(char *command, char **envp);
 static t_list	*process_infile(char **args, int *index, t_command_type type);
-char			*format_path(char *command);
 t_command_type	get_cmd_type(char **args, int argc, int index);
 
-t_list	*process(int argc, char **args)
+t_list	*process(int argc, char **args, char **envp)
 {
 	t_list	*cmds;
 	t_list	*new_cmd_node;
@@ -31,10 +30,9 @@ t_list	*process(int argc, char **args)
 			new_cmd_node = process_infile(args, &i, get_cmd_type(args, argc,
 						i));
 		else
-			new_cmd_node = process_commands(args[i]);
+			new_cmd_node = process_commands(args[i], envp);
 		if (!new_cmd_node)
 		{
-			errno = ENOMEM;
 			ft_lstclear(&cmds, free);
 			return (NULL);
 		}
@@ -43,46 +41,20 @@ t_list	*process(int argc, char **args)
 	return (cmds);
 }
 
-char	*format_path(char *command)
-{
-	char	*path_name;
-	char	*tmp;
-	int		i;
-
-	i = 0;
-	while (command[i])
-	{
-		if (ft_isspace(command[i]))
-			break ;
-		i++;
-	}
-	if (command[i])
-		path_name = ft_substr(command, 0, i);
-	else
-		path_name = ft_strdup(command);
-	if (!path_name)
-		return (NULL);
-	tmp = path_name;
-	path_name = ft_strjoin("/bin/", path_name);
-	free(tmp);
-	return (path_name);
-}
-
-t_list	*process_commands(char *command)
+t_list	*process_commands(char *cmd_arg, char **envp)
 {
 	t_list	*processed;
 	t_cmd	*cmd;
 
 	cmd = malloc(sizeof(t_cmd));
 	if (!cmd)
-		return (NULL);
-	cmd->type = CMD;
-	cmd->cmd = ft_split(command, ' ');
+		return set_errno(ENOMEM);
+	cmd->cmd = ft_split(cmd_arg, ' ');
 	if (!cmd->cmd)
-		return (NULL);
-	cmd->path_name = format_path(command);
-	if (!cmd->path_name)
-		return (NULL);
+		return set_errno(ENOMEM);
+	cmd->type = CMD;
+	cmd->envp = envp;
+	cmd->path_name = format_path(cmd->cmd[0], envp);
 	processed = ft_lstnew(cmd);
 	return (processed);
 }
@@ -98,6 +70,7 @@ t_list	*process_infile(char **args, int *index, t_command_type type)
 		*index += 1;
 	cmd->path_name = ft_strdup(args[*index]);
 	cmd->cmd = NULL;
+	cmd->envp = NULL;
 	processed = ft_lstnew(cmd);
 	return (processed);
 }
