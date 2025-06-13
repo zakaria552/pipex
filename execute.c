5f074cbc0003143745f4059befe2682757115cf2
Zakaria Farah <zakaria.farah55@outlook.com>
2025-06-13 18:06:41 +0300

    leak fixes

--- execute.c
+++ execute.c
@@ -6,7 +6,7 @@
 /*   By: zfarah <zfarah@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 21:25:07 by zfarah            #+#    #+#             */
-/*   Updated: 2025/06/13 13:59:21 by zfarah           ###   ########.fr       */
+/*   Updated: 2025/06/13 18:00:49 by zfarah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,24 @@ void	execute(t_cmd *command, int pipes[][2])
 	char	*path;
 
 	path = format_path(*(command->cmd), command->envp);
-	if (!path)
-		return ;
 	current_pipe = pipes[0];
 	next_pipe = pipes[1];
+	if (!path)
+	{
+		close_pipe(current_pipe);
+		close_pipe(next_pipe);
+		return ;
+	}
 	if (dup2(current_pipe[0], STDIN_FILENO) < 0 || dup2(next_pipe[1],
 			STDOUT_FILENO) < 0)
+	{
+		free(path);
+		close_pipe(current_pipe);
+		close_pipe(next_pipe);
 		return ;
+	}
 	close_pipe(current_pipe);
 	close_pipe(next_pipe);
 	execve(path, command->cmd, command->envp);
+	free(path);
 }
