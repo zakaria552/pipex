3a62fe8af89b21c35c14bda54a18ceacee5e6b2e
Zakaria Farah <zakaria.farah55@outlook.com>
2025-08-11 10:48:30 +0300

    finalized

---
+++ src/execute.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zfarah <zfarah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 21:25:07 by zfarah            #+#    #+#             */
+/*   Updated: 2025/08/11 10:38:17 by zfarah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipe.h"
+
+void	execute(t_cmd *command, int pipes[][2])
+{
+	int		*current_pipe;
+	int		*next_pipe;
+	char	*path;
+
+	path = format_path(*(command->cmd), command->envp);
+	current_pipe = pipes[0];
+	next_pipe = pipes[1];
+	if (!path)
+	{
+		close_pipe(current_pipe);
+		close_pipe(next_pipe);
+		return ;
+	}
+	if (dup2(current_pipe[0], STDIN_FILENO) < 0 || dup2(next_pipe[1],
+			STDOUT_FILENO) < 0)
+	{
+		free(path);
+		close_pipe(current_pipe);
+		close_pipe(next_pipe);
+		return ;
+	}
+	close_pipe(current_pipe);
+	close_pipe(next_pipe);
+	execve(path, command->cmd, command->envp);
+	free(path);
+}
